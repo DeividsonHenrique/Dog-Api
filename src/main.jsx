8ad553e3bce80cbed33b75724b5dafb3d62dc0ae
Deividson Henry <109252541+DeividsonHenrique@users.noreply.github.com>
2024-11-14 22:45:35 -0300

    day 14.11.24

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 import { CardProvider } from "./CreateContext.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <CardProvider>
       <App />
     </CardProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
